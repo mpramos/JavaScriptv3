059d806e6afe78a8637ccdbd5effe850b35ded9c
PaolaRamosGutierrez <paolagutierrezramos5@gmail.com>
2022-10-12 21:22:44 -0400

    uso de Let y Var

---
+++ LetVsConst.js
@@ -0,0 +1,75 @@
+console.log('especialidad de javascript')
+console.log('especialidad de React')
+console.log('especialidad de python')
+
+{
+    console.log('Hola ')
+    console.log('Hola ')
+    console.log('Hola ')
+}
+{
+    // int n=4
+    console.log(typeof nombre);
+    var nombre="susana" //?
+}
+//! var let const 
+//? var VS let 
+nombre//?
+var nombreEst="andre"
+nombreEst
+{
+    nombreEst
+}
+{
+    nombreEst
+}
+//* ----------------------------
+{
+    var edadShakira=45 //?
+}
+edadShakira
+console.log(edadShakira);
+//* ----------------------------
+
+let apellido='Bejarano'
+{
+    console.log(apellido);
+    apellido
+}
+{
+    console.log(apellido);
+    apellido
+}
+//*-------------------------------
+let apellidoShakira='Mebarak'
+{
+    let apellidoShakira='Mebarak'
+    console.log(apellidoShakira);
+}
+apellidoShakira //?
+//* ----------------------------------
+var nombreImportanteS='Shakira' //?
+typeof(nombreImportanteS)//?
+var nombreImportanteL="Leonardo Dicaprio" //?
+typeof(nombreImportanteL)//?
+// var nombreImportanteSL='Shakira' + ' '+ 'Leonardo'  //?
+var nombreImportanteSL=`${nombreImportanteS} ${nombreImportanteL} ${"andre"} ${"marisol"}`  //?
+nombreImportanteSL
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
